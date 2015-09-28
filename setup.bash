438b36006479838fa5731b08795ae6d5a7a3fa08
Alex Jensen <96dalex@gmail.com>
2015-09-28 15:06:27 -0600

    rspec config file

--- setup.bash
+++ setup.bash
@@ -1,6 +1,7 @@
 ln -s $PWD/bash_profile ~/.bash_profile
 ln -s $PWD/aliases.bash ~/.aliases.bash
 ln -s $PWD/vimrc ~/.vimrc
+ln -s $PWD/rspec ~/.rspec
 
 if [ ! -d ~/.ruby_scripts ]; then
   ln -s $PWD/ruby_scripts ~/.ruby_scripts
