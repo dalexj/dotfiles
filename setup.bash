75c08e7d5615741e42acb51dad2a97fa29cff437
Alex Jensen <alex.jensen@hotelengine.com>
2020-07-22 15:35:51 -0600

    remove vim

--- setup.bash
+++ setup.bash
@@ -1,16 +1,11 @@
 ln -s $PWD/bash_profile ~/.bash_profile
 ln -s $PWD/aliases.bash ~/.aliases.bash
-ln -s $PWD/vimrc ~/.vimrc
 ln -s $PWD/rspec ~/.rspec
 ln -s $PWD/mix_autocomplete/mix_autocomplete.sh ~/.mix-autocomplete.sh
 ln -s $PWD/hk-bash-completion.sh ~/.hk-bash-completion.sh
 
 if [ ! -d ~/.ruby_scripts ]; then
   ln -s $PWD/ruby_scripts ~/.ruby_scripts
-fi
-
-if [ ! -d ~/.vim ]; then
-  ln -s $PWD/vim ~/.vim
 fi
 
 # git submodule update --init --recursive
