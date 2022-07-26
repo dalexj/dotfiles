3fc3222cf823b106e599083e76276eaf0b8e2580
Alex Jensen <alexjensen@Hotels-MacBook-Pro.local>
2022-07-26 10:24:39 -0600

    add pryrc and psqlrc to setup

--- setup.bash
+++ setup.bash
@@ -1,7 +1,9 @@
 ln -s $PWD/bash_profile ~/.bash_profile
 ln -s $PWD/aliases.bash ~/.aliases.bash
 ln -s $PWD/rspec ~/.rspec
-ln -s $PWD/mix_autocomplete/mix_autocomplete.sh ~/.mix-autocomplete.sh
+ln -s $PWD/pryrc ~/.pryrc
+ln -s $PWD/psqlrc ~/.psqlrc
+# ln -s $PWD/mix_autocomplete/mix_autocomplete.sh ~/.mix-autocomplete.sh
 ln -s $PWD/hk-bash-completion.sh ~/.hk-bash-completion.sh
 
 if [ ! -d ~/.ruby_scripts ]; then
